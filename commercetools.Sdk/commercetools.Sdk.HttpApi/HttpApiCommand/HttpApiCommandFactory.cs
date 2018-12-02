1843d37c683c4f380627787a0bf4ea86657c9d8a
Katarina Stojanovski <stojanovski.katarina@gmail.com>
2018-12-02 18:18:59 +0100

    replaced activator with compiled lamba ctor

--- commercetools.Sdk/commercetools.Sdk.HttpApi/HttpApiCommand/HttpApiCommandFactory.cs
+++ commercetools.Sdk/commercetools.Sdk.HttpApi/HttpApiCommand/HttpApiCommandFactory.cs
@@ -3,12 +3,16 @@ using commercetools.Sdk.Util;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 
 namespace commercetools.Sdk.HttpApi
 {
     public class HttpApiCommandFactory : IHttpApiCommandFactory
     {
         private IEnumerable<Type> registeredHttpApiCommandTypes;
+        private readonly Dictionary<Type, ObjectActivator> activators;
+
         private IRequestMessageBuilderFactory requestMessageBuilderFactory;
 
         public HttpApiCommandFactory(IRegisteredTypeRetriever registeredTypeRetriever, IRequestMessageBuilderFactory requestMessageBuilderFactory)
@@ -16,6 +20,51 @@ namespace commercetools.Sdk.HttpApi
             IEnumerable<Type> registeredHttpApiCommandTypes = registeredTypeRetriever.GetRegisteredTypes<IHttpApiCommand>();
             this.registeredHttpApiCommandTypes = registeredHttpApiCommandTypes;
             this.requestMessageBuilderFactory = requestMessageBuilderFactory;
+            this.activators = new Dictionary<Type, ObjectActivator>();
+        }
+
+        private delegate object ObjectActivator(params object[] args);
+
+        private ObjectActivator GetActivator(ConstructorInfo ctor)
+        {
+            Type type = ctor.DeclaringType;
+            ParameterInfo[] paramsInfo = ctor.GetParameters();
+
+            //create a single param of type object[]
+            ParameterExpression param =
+                Expression.Parameter(typeof(object[]), "args");
+
+            Expression[] argsExp =
+                new Expression[paramsInfo.Length];
+
+            //pick each arg from the params array
+            //and create a typed expression of them
+            for (int i = 0; i < paramsInfo.Length; i++)
+            {
+                Expression index = Expression.Constant(i);
+                Type paramType = paramsInfo[i].ParameterType;
+
+                Expression paramAccessorExp =
+                    Expression.ArrayIndex(param, index);
+
+                Expression paramCastExp =
+                    Expression.Convert(paramAccessorExp, paramType);
+
+                argsExp[i] = paramCastExp;
+            }
+
+            //make a NewExpression that calls the
+            //ctor with the args we just created
+            NewExpression newExp = Expression.New(ctor, argsExp);
+
+            //create a lambda with the New
+            //Expression as body and our param object[] as arg
+            LambdaExpression lambda =
+                Expression.Lambda(typeof(ObjectActivator), newExp, param);
+
+            //compile it
+            ObjectActivator compiled = (ObjectActivator)lambda.Compile();
+            return compiled;
         }
 
         public IHttpApiCommand Create<T>(Command<T> command)
@@ -46,8 +95,23 @@ namespace commercetools.Sdk.HttpApi
                 }
             }
 
-            // TODO Replace with compiled lamba expression
-            return Activator.CreateInstance(httApiCommandType.MakeGenericType(typeOfGeneric), command, requestMessageBuilderFactory) as IHttpApiCommand;
+            Type requestedType = httApiCommandType.MakeGenericType(typeOfGeneric);
+            ObjectActivator createdActivator;
+            if (activators.ContainsKey(requestedType))
+            {
+                createdActivator = activators[requestedType];
+            }
+            else
+            {
+                ConstructorInfo ctor = requestedType.GetConstructors().First();
+                createdActivator = GetActivator(ctor);
+                activators[requestedType] = createdActivator;
+            }
+
+            object instance = createdActivator(command, requestMessageBuilderFactory);
+            return instance as IHttpApiCommand;
         }
+
+
     }
 }
