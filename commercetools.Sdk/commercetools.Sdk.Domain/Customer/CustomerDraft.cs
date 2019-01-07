d272b5a76656e84bc70bc3958950f1b3514a9876
sshibani <siawash.shibani@commercetools.com>
2019-01-07 19:58:17 +0100

    add CustomerDraft object

---
+++ commercetools.Sdk/commercetools.Sdk.Domain/Customer/CustomerDraft.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace commercetools.Sdk.Domain
+{
+    public class CustomerDraft :IDraft<Customer>
+    {
+        public string CustomerNumber { get; set; }
+        public string Key { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string Title { get; set; }
+        public string Salutation { get; set; }
+        public string DateOfBirth { get; set; }
+        public string CompanyName { get; set; }
+        public string VatId { get; set; }
+        public List<Address> Addresses { get; set; }
+        public string DefaultShippingAddressId { get; set; }
+        public List<string> ShippingAddressIds { get; set; }
+        public string DefaultBillingAddressId { get; set; }
+        public List<string> BillingAddressIds { get; set; }
+        public bool IsEmailVerified { get; set; }
+        public string ExternalId { get; set; }
+        public Reference CustomerGroup { get; set; }
+        public CustomFields Custom { get; set; }
+        public string Locale { get; set; }
+    }
+}
