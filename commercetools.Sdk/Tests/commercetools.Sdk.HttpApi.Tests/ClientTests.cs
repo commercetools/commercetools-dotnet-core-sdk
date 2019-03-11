24699e0a90238d13ab81f8b586c19026f89fbcb1
Michele George <michele.george@commercetools.de>
2019-03-11 12:37:28 +0100

    Added UserAgent to the Http Client

--- commercetools.Sdk/Tests/commercetools.Sdk.HttpApi.Tests/ClientTests.cs
+++ commercetools.Sdk/Tests/commercetools.Sdk.HttpApi.Tests/ClientTests.cs
@@ -9,6 +9,7 @@ using Moq.Protected;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading;
@@ -50,6 +51,32 @@ namespace commercetools.Sdk.HttpApi.Tests
             string categoryId = "2bafc816-4223-4ff0-ac8a-0f08a8f29fd6";
             Category category = commerceToolsClient.ExecuteAsync(new GetByIdCommand<Category>(new Guid(categoryId))).Result;
             Assert.Equal(categoryId, category.Id.ToString());
+        }
+
+        [Fact(Skip = "Need to be refactored and written in better way")]
+        public void TestUserAgent()
+        {
+            var mockHttpClientFactory = new Mock<IHttpClientFactory>();
+            var mockHandler = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+            mockHttpClientFactory.Setup(x => x.CreateClient(DefaultClientNames.Api)).Returns(new HttpClient(mockHandler.Object));
+            mockHandler
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>()
+                )
+                .ReturnsAsync(new HttpResponseMessage()
+                {
+                    StatusCode = HttpStatusCode.OK
+                })
+                .Verifiable();
+            Client commerceToolsClient = new Client(mockHttpClientFactory.Object, this.clientFixture.GetService<IHttpApiCommandFactory>(), this.clientFixture.GetService<ISerializerService>());
+            //var userAgent = commerceToolsClient.GetClientUserAgent();
+            var userAgent = "";
+            //TODO: Check UserAgent with Regular Expression
+            //Assert.Equal("commercetools-dotnet-core-sdk/1.0.0.0 .NET-core/4.6.45454", userAgent.DefaultRequestHeaders.UserAgent.First().ToString());
+            Assert.Equal("commercetools-dotnet-core-sdk/1.0.0.0 .NET-core/4.6.45454", userAgent);
         }
     }
 }
--- commercetools.Sdk/commercetools.Sdk.HttpApi/Client.cs
+++ commercetools.Sdk/commercetools.Sdk.HttpApi/Client.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Net.Http;
+﻿using System.Net.Http;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using commercetools.Sdk.Client;
-using commercetools.Sdk.HttpApi.Domain;
 using commercetools.Sdk.HttpApi.Domain.Exceptions;
 using commercetools.Sdk.Serialization;
 
@@ -31,6 +31,7 @@ namespace commercetools.Sdk.HttpApi
                 if (this.httpClient == null)
                 {
                     this.httpClient = this.httpClientFactory.CreateClient(this.Name);
+                    this.httpClient.DefaultRequestHeaders.UserAgent.ParseAdd(this.GetClientUserAgent());
                 }
 
                 return this.httpClient;
@@ -55,6 +56,20 @@ namespace commercetools.Sdk.HttpApi
             // it will not reach this because either it will success and return the deserialized object or fail and handled by ErrorHandler which will throw it using the right exception type
             var exception = new ApiException(result.ReasonPhrase);
             throw exception;
+        }
+
+        /// <summary>
+        /// Get UserAgent which we will set in the http client
+        /// </summary>
+        /// <returns>user agent with assembly version and .Net version</returns>
+        private string GetClientUserAgent()
+        {
+            string assemblyVersion = Assembly
+                .GetExecutingAssembly().GetName().Version.ToString();
+            //ToDO: Make dotnet version string in better way, it now return (commercetools-dotnet-core-sdk/1.0.0.0 .NET Core 4.6.27317.03) and should return something like ( commercetools-dotnet-core-sdk/1.0.0.0 .NET-Core/4.6.27317.03)
+            string dotNetVersion = RuntimeInformation.FrameworkDescription;
+            string userAgent = $"commercetools-dotnet-core-sdk/{assemblyVersion} {dotNetVersion}";
+            return userAgent;
         }
     }
 }
