c0c0334d5a5b487ba449eb0de2b9fc2d50fc4b34
Jens Schulze <jens.schulze@commercetools.de>
2019-03-22 13:37:59 +0100

    Automatic versioning (#12)
    * test automatic versioning
    
    * fix appveyor config
    
    * update version script
    
    * remove unnecessary environment check
    
    * remove version from appveyor config file
    
    * rename versioning script
    
    * better version output
    
    * patch assembly to build version
    
    * fix appveyor config
    
    * include build number for tags
    update build version variable
    
    * update build version variable
    
    * move version change to init phase
    
    * inline version script
    
    * test
    
    * try install hook
    
    * fetch base version from appveyor config
    
    * fix PR version string

---
+++ set_appveyor_version.ps1
@@ -0,0 +1,60 @@
+#$env:APPVEYOR_REPO_TAG = "false"
+#$env:APPVEYOR_REPO_TAG_NAME = "1.2.0"
+#$env:APPVEYOR_BUILD_NUMBER = "1"
+#$env:APPVEYOR_PULL_REQUEST_NUMBER = ""
+#$env:APPVEYOR_REPO_BRANCH = "dfd f"
+#$env:CONFIGURATION = "Release"
+#$env:APPVEYOR_BUILD_VERSION = "1.0.0.1"
+
+function getVersion() {
+    $version = $env:APPVEYOR_BUILD_VERSION.split('.')[0..2] -join '.';
+    $dbgSuffix = If ($env:CONFIGURATION -eq "Debug") { "-dbg" } else { "" };
+    $buildNumber = $env:APPVEYOR_BUILD_NUMBER;
+
+    if ($env:APPVEYOR_REPO_TAG -eq "true")
+    {
+        $packageVersion = $env:APPVEYOR_REPO_TAG_NAME + "." + $buildNumber;
+    }
+    else
+    {
+        $branch = $env:APPVEYOR_REPO_BRANCH;
+        $noPR = [string]::IsNullOrEmpty($env:APPVEYOR_PULL_REQUEST_NUMBER);
+
+        if ($branch -eq "master" -And $noPR)
+        {
+            $packageVersion = $version + "-dev-" + $buildNumber + $dbgSuffix;
+        }
+        else
+        {
+            $suffix = "-ci-" + $buildNumber + $dbgSuffix;
+
+            if (!$noPR)
+            {
+                $suffix += "-pr-" + $env:APPVEYOR_PULL_REQUEST_NUMBER;
+            }
+            elseif ($branch.StartsWith("release", "CurrentCultureIgnoreCase"))
+            {
+                $suffix += "-pre-" + $buildNumber;
+            }
+            else
+            {
+                $safeBranch = $branch -replace "[^0-9A-Za-z-]+", ""
+                $suffix += "-" + $safeBranch;
+            }
+            # Nuget limits "special version part" to 20 chars. Add one for the hyphen.
+            if ($suffix.Length > 21)
+            {
+                $suffix = $suffix.Substring(0, 21);
+            }
+
+            $packageVersion = $version + $suffix;
+        }
+    }
+
+    return $packageVersion;
+}
+
+
+$version = getVersion
+echo "Build version: $version"
+Update-AppveyorBuild -Version $version
