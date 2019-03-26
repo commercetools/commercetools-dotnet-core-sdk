0e3174b66537b1212ab0782f0c6860aa9ff468c6
Michele George <michele.george@commercetools.de>
2019-03-26 17:13:00 +0100

    Added ReplicateHttpApiCommand and ReplicateRequestMessageBuilder for Replicate Command

---
+++ commercetools.Sdk/commercetools.Sdk.HttpApi/HttpApiCommands/ReplicateHttpApiCommand.cs
@@ -0,0 +1,21 @@
+﻿using commercetools.Sdk.HttpApi.RequestBuilders;
+
+namespace commercetools.Sdk.HttpApi.HttpApiCommands
+{
+    using System.Net.Http;
+    using commercetools.Sdk.Client;
+
+    public class ReplicateHttpApiCommand<T> : IHttpApiCommandGeneric<ReplicateCommand<T>, T>
+    {
+        private readonly ReplicateCommand<T> command;
+        private readonly ReplicateRequestMessageBuilder requestBuilder;
+
+        public ReplicateHttpApiCommand(ReplicateCommand<T> command, IRequestMessageBuilderFactory requestMessageBuilderFactory)
+        {
+            this.command = command;
+            this.requestBuilder = requestMessageBuilderFactory.GetRequestMessageBuilder<ReplicateRequestMessageBuilder>();
+        }
+
+        public HttpRequestMessage HttpRequestMessage => this.requestBuilder.GetRequestMessage(this.command);
+    }
+}
