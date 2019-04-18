9be04c27be9ac1b2cbd35d11339e612aeffcc88c
Christian Neuß <christian@neuss.consulting>
2019-04-18 14:35:31 +0200

    Remove not working projects from solution.

---
+++ commercetools.Sdk/commercetools.Sdk.DependencyInjection.SimpleInjector/DependencyInjectionSetup.cs
@@ -0,0 +1,10 @@
+﻿namespace commercetools.Sdk.DependencyInjection.SimpleInjector
+{
+    /// <summary>
+    /// Contains extensions methods to use when setting up commercetools dependencies.
+    /// </summary>
+    public class DependencyInjectionSetup
+    {
+
+    }
+}
