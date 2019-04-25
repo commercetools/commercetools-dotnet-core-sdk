6ab48e8fe0ceb625f69c99f53c979553f0cf755b
Michele George <michele.george@commercetools.de>
2019-04-25 09:35:15 +0200

    use Resource<T> for custom object

--- commercetools.Sdk/commercetools.Sdk.Domain/CustomObject/CustomObjectBase.cs
+++ commercetools.Sdk/commercetools.Sdk.Domain/CustomObject/CustomObjectBase.cs
@@ -1,19 +1,12 @@
 using System;
+using commercetools.Sdk.Domain.Common;
 
 namespace commercetools.Sdk.Domain.CustomObject
 {
-    public abstract class CustomObjectBase
+    public abstract class CustomObjectBase : Resource<CustomObjectBase>
     {
-        public string Id { get; set; }
-
         public string Container { get; set; }
 
         public string Key { get; set; }
-
-        public int Version { get; set; }
-
-        public DateTime CreatedAt { get; set; }
-
-        public DateTime LastModifiedAt { get; set; }
     }
 }
