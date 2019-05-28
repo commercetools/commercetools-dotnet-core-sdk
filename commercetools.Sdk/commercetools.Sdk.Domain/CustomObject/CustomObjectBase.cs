49e624be9c97622119e0b0a891dc97e47ae9fc6d
Jens Schulze <jenschude@gmail.com>
2019-05-28 14:49:53 +0200

    fix reference deserialization for custom fields fix custom object references

--- commercetools.Sdk/commercetools.Sdk.Domain/CustomObject/CustomObjectBase.cs
+++ commercetools.Sdk/commercetools.Sdk.Domain/CustomObject/CustomObjectBase.cs
@@ -3,6 +3,8 @@ using commercetools.Sdk.Domain.Common;
 
 namespace commercetools.Sdk.Domain.CustomObject
 {
+    [Endpoint("custom-objects")]
+    [ResourceType(ReferenceTypeId.KeyValueDocument)]
     public abstract class CustomObjectBase : Resource<CustomObjectBase>
     {
         public string Container { get; set; }
