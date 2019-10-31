310a1582b9af1738e20f27bab4695fb89a7abb48
Jens Schulze <jenschude@gmail.com>
2019-10-31 11:27:25 +0100

    add Integration test for Serialization of Domain Models with system.text.json

---
+++ commercetools.Sdk/IntegrationTests/commercetools.Sdk.Core3Tests/IntegrationTestCollection.cs
@@ -0,0 +1,10 @@
+using Xunit;
+
+namespace commercetools.Sdk.Core3Tests
+{
+    [CollectionDefinition("Integration Tests")]
+    public class IntegrationTestCollection : ICollectionFixture<ServiceProviderFixture>
+    {
+
+    }
+}
