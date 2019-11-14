d1ea7a810575e375be638fee8f3d1f0bd48e6e54
Michele George <michele.george@commercetools.de>
2019-11-14 14:59:26 +0100

    Adding MvcExample project to List Products as Example of how to use SDK in MVC Application

---
+++ commercetools.Sdk/Examples/commercetools.Sdk.MvcExample/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using commercetools.Sdk.Client;
+using commercetools.Sdk.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using commercetools.Sdk.MvcExample.Models;
+
+namespace commercetools.Sdk.MvcExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IClient client;
+        
+        public HomeController(IClient client)
+        {
+            this.client = client;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            //Get All Products
+            var productsQueryResult = await client.ExecuteAsync(new QueryCommand<Product>());
+            var products = productsQueryResult.Results;
+            return View(products);
+        }
+        
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+    }
+}
---
+++ commercetools.Sdk/Examples/commercetools.Sdk.MvcExample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace commercetools.Sdk.MvcExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
