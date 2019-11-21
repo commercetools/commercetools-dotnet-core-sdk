b7ebde2cfb401ecc680f54ba33587335db37d7a2
Michele George <michele.george@commercetools.de>
2019-11-21 14:41:31 +0100

    Adding "Key" Property to ProductDiscountDraft and ProductDiscount Models

--- commercetools.Sdk/commercetools.Sdk.Domain/ProductDiscounts/ProductDiscount.cs
+++ commercetools.Sdk/commercetools.Sdk.Domain/ProductDiscounts/ProductDiscount.cs
@@ -6,16 +6,20 @@ namespace commercetools.Sdk.Domain.ProductDiscounts
 {
     [Endpoint("product-discounts")]
     [ResourceType(ReferenceTypeId.ProductDiscount)]
-    public class ProductDiscount : Resource<ProductDiscount>
+    public class ProductDiscount : Resource<ProductDiscount>, IKeyReferencable<ProductDiscount>
     {
         public LocalizedString Name { get; set; }
+        
+        public string Key { get; set; }
         public LocalizedString Description { get; set; }
+        
         public ProductDiscountValue Value { get; set; }
         public string Predicate { get; set; }
         public string SortOrder { get; set; }
         public bool IsActive { get; set; }
         public DateTime ValidFrom { get; set; }
         public DateTime ValidUntil { get; set; }
+        
         public List<Reference> References { get; set; }
     }
 }
