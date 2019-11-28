44dd4e30bebac9b639f50bd5a7ae1dfd473d99ff
Michele George <michele.george@commercetools.de>
2019-11-28 13:22:32 +0100

    Adding SetKeyUpdateAction Model for ProductDiscount endpoint

---
+++ commercetools.Sdk/commercetools.Sdk.Domain/ProductDiscounts/UpdateActions/SetKeyUpdateAction.cs
@@ -0,0 +1,8 @@
+﻿namespace commercetools.Sdk.Domain.ProductDiscounts.UpdateActions
+{
+    public class SetKeyUpdateAction : UpdateAction<ProductDiscount>
+    {
+        public string Action => "setKey";
+        public string Key { get; set; }
+    }
+}
