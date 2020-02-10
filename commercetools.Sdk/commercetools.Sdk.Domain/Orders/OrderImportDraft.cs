aa1dd8680378371eb1471540e6b7a7095073465e
Michele George <michele.george@commercetools.de>
2020-02-10 15:39:13 +0100

    Adding the store field to the OrderImportDraft

--- commercetools.Sdk/commercetools.Sdk.Domain/Orders/OrderImportDraft.cs
+++ commercetools.Sdk/commercetools.Sdk.Domain/Orders/OrderImportDraft.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Text;
 using commercetools.Sdk.Domain.Carts;
 using commercetools.Sdk.Domain.CustomerGroups;
+using commercetools.Sdk.Domain.Stores;
 using commercetools.Sdk.Domain.Validation.Attributes;
 
 namespace commercetools.Sdk.Domain.Orders
@@ -37,5 +38,7 @@ namespace commercetools.Sdk.Domain.Orders
         public TaxCalculationMode TaxCalculationMode { get; set; }
         public CartOrigin Origin { get; set; }
         public List<Address> ItemShippingAddresses { get; set; }
+        
+        public ResourceIdentifier<Store> Store { get; set; }
     }
 }
