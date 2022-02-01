8bc9b92267ed70df9c6449af5a6e3e7aa3405780
Michele George <michele.george@commercetools.de>
2022-02-01 16:05:11 +0100

    Added Set Address Custom Type and Set Address Custom Field update actions on customers and Channels

---
+++ commercetools.Sdk/commercetools.Sdk.Domain/Channels/UpdateActions/SetAddressCustomFieldUpdateAction.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace commercetools.Sdk.Domain.Channels.UpdateActions
+{
+    public class SetAddressCustomFieldUpdateAction : UpdateAction<Channel>
+    {
+        public string Action => "setAddressCustomField";
+        [Required]
+        public string Name { get; set; }
+        public object Value { get; set; }
+    }
+}
---
+++ commercetools.Sdk/commercetools.Sdk.Domain/Channels/UpdateActions/SetAddressCustomTypeUpdateAction.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace commercetools.Sdk.Domain.Channels.UpdateActions
+{
+    public class SetAddressCustomTypeUpdateAction : UpdateAction<Channel>
+    {
+        public string Action => "setAddressCustomType";
+        public ResourceIdentifier Type { get; set; }
+        public Fields Fields { get; set; }
+    }
+}
