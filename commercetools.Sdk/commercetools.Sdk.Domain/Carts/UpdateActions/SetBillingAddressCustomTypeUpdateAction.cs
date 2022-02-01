8ed5b02a47f9d31a4ab186ed29d149b6875489e0
Michele George <michele.george@commercetools.de>
2022-02-01 13:38:05 +0100

    Added Set Billing Address Custom Type and Set Billing Address Custom Field update actions for carts

---
+++ commercetools.Sdk/commercetools.Sdk.Domain/Carts/UpdateActions/SetBillingAddressCustomTypeUpdateAction.cs
@@ -0,0 +1,11 @@
+﻿namespace commercetools.Sdk.Domain.Carts.UpdateActions
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class SetBillingAddressCustomTypeUpdateAction : UpdateAction<Cart>
+    {
+        public string Action => "setBillingAddressCustomType";
+        public ResourceIdentifier Type { get; set; }
+        public Fields Fields { get; set; }
+    }
+}
