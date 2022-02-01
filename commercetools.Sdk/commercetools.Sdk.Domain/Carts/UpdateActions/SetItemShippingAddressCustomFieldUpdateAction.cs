42aa14cd45355e229a3da33ba67fbf75dfc01913
Michele George <michele.george@commercetools.de>
2022-02-01 14:41:05 +0100

    Added Set Item Shipping Address Custom Type and Set Item Shipping Address Custom Field update actions for carts

---
+++ commercetools.Sdk/commercetools.Sdk.Domain/Carts/UpdateActions/SetItemShippingAddressCustomFieldUpdateAction.cs
@@ -0,0 +1,12 @@
+﻿namespace commercetools.Sdk.Domain.Carts.UpdateActions
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class SetItemShippingAddressCustomFieldUpdateAction : UpdateAction<Cart>
+    {
+        public string Action => "setItemShippingAddressCustomField";
+        [Required]
+        public string Name { get; set; }
+        public object Value { get; set; }
+    }
+}
---
+++ commercetools.Sdk/commercetools.Sdk.Domain/Carts/UpdateActions/SetItemShippingCustomTypeUpdateAction.cs
@@ -0,0 +1,11 @@
+﻿namespace commercetools.Sdk.Domain.Carts.UpdateActions
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class SetItemShippingAddressCustomTypeUpdateAction : UpdateAction<Cart>
+    {
+        public string Action => "setItemShippingAddressCustomType";
+        public ResourceIdentifier Type { get; set; }
+        public Fields Fields { get; set; }
+    }
+}
