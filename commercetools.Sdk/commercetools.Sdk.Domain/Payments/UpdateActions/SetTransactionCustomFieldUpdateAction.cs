705b5c08909aff3e7ae9b140909c12d9b427818b
Michele George <michele.george@commercetools.de>
2022-02-07 11:36:41 +0100

    Payment transactions can now be extended with Custom Fields

---
+++ commercetools.Sdk/commercetools.Sdk.Domain/Payments/UpdateActions/SetTransactionCustomFieldUpdateAction.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace commercetools.Sdk.Domain.Payments.UpdateActions
+{
+    public class SetTransactionCustomFieldUpdateAction : UpdateAction<Payment>
+    {
+        public string Action => "setTransactionCustomField";
+        [Required]
+        public string Name { get; set; }
+        public object Value { get; set; }
+        [Required]
+        public string TransactionId { get; set; }
+    }
+}
---
+++ commercetools.Sdk/commercetools.Sdk.Domain/Payments/UpdateActions/SetTransactionCustomTypeUpdateAction.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace commercetools.Sdk.Domain.Payments.UpdateActions
+{
+    public class SetTransactionCustomTypeUpdateAction : UpdateAction<Payment>
+    {
+        public string Action => "setTransactionCustomType";
+        public ResourceIdentifier Type { get; set; }
+        public Fields Fields { get; set; }
+        [Required]
+        public string TransactionId { get; set; }
+    }
+}
