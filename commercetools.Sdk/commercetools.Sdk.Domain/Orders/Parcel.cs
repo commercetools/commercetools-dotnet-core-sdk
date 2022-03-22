4b17b1a66429319b26e24f51f6770dc3c964c8bb
Michele George <michele.george@commercetools.de>
2022-03-22 11:50:23 +0100

    Parcels can now be extended with Custom Fields

--- commercetools.Sdk/commercetools.Sdk.Domain/Orders/Parcel.cs
+++ commercetools.Sdk/commercetools.Sdk.Domain/Orders/Parcel.cs
@@ -10,5 +10,6 @@ namespace commercetools.Sdk.Domain.Orders
         public ParcelMeasurements Measurements { get; set; }
         public TrackingData TrackingData { get; set; }
         public List<DeliveryItem> Items { get; set; }
+        public CustomFields Custom { get; set; }
     }
 }
---
+++ commercetools.Sdk/commercetools.Sdk.Domain/Orders/UpdateActions/SetParcelCustomTypeUpdateAction.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace commercetools.Sdk.Domain.Orders.UpdateActions
+{
+    public class SetParcelCustomTypeUpdateAction : OrderUpdateAction
+    {
+        public override string Action => "setParcelCustomType";
+        [Required]
+        public string ParcelId { get; set; }
+        public ResourceIdentifier Type { get; set; }
+        public Fields Fields { get; set; }
+    }
+}
